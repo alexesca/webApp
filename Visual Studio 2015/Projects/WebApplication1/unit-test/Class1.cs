96292c4809410514a72c9d70dba9ec0402927995
alexesca <alexander.escamilla.moreno@gmail.com>
2016-07-08 13:49:29 -0600

    adding a failing test.

---
+++ Visual Studio 2015/Projects/WebApplication1/unit-test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+namespace unit_test
+{
+    public class Class1
+    {
+        [fact]
+        public void failingTest()
+        {
+            Assert.False(true);
+        }
+    }
+}
